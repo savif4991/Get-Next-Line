c62d5a919b91e25d33c69eea775e55b3cfaf3396
daejin <savif4991@gmail.com>
2022-06-25 17:08:44 +0900

    commit

--- get_next_line.c
+++ get_next_line.c
@@ -1,9 +1,83 @@
 #include "get_next_line.h"
 
-/*
+char	*get_res(unsigned int new_line_idx, char *buf)
+{
+	char			*res;
+	unsigned int	i;
 
-*/
+	res = (char *)malloc(new_line_idx);
+	if (res == NULL)
+		return (NULL);
+	i = new_line_idx;
+	while (i--)
+		res[i] = buf[i];
+	free (buf);
+	return (res);
+}
+
+unsigned int	seek_new_line(char *buf, unsigned int *flag)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (buf[i] != '\n')
+	{
+		i++;
+		if (!buf[i])
+		{
+			*flag = 0;
+			return (i);
+		}
+	}
+	return (i);
+}
+
+char	*get_more_byte(char *buf, int fd, unsigned int *new_line_idx)
+{
+	char				*res;
+	unsigned int		total_size;
+	static unsigned int	count = 2;
+	unsigned int		i;
+	unsigned int		flag;
+
+	total_size = BUFFER_SIZE * count;
+	i = total_size - BUFFER_SIZE;
+	flag = 1;
+	res = (char *)malloc(total_size);
+	if (res == NULL)
+		return (NULL);
+	while (i)
+		res[i] = buf[i--];
+	free (buf);
+	read(fd, res[total_size - BUFFER_SIZE], BUFFER_SIZE);
+	i = seek_new_line(res, &flag);
+	if (flag)
+	{
+		*new_line_idx += i;
+		return (get_res(*new_line_idx, res));
+	}
+	else
+	{
+		count++;
+		*new_line_idx += BUFFER_SIZE;
+		get_more_byte(res, fd, new_line_idx);
+	}
+}
+
 char	*get_next_line(int fd)
 {
+	char				*buf;
+	static unsigned int	new_line_idx = 0;
+	unsigned int		flag;
 
+	flag = 1;
+	buf = (char*)malloc(BUFFER_SIZE);
+	if (buf == 0)
+		return (NULL);
+	read(fd, buf, BUFFER_SIZE);
+	new_line_idx += seek_new_line(buf, &flag);
+	if (flag)
+		return (get_res(new_line_idx, buf));
+	else
+		return (get_more_byte(buf, fd, &new_line_idx));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,13 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
+# include <unistd.h>
 
+# define BUFFER_SIZE 256
 
+char	*get_res(unsigned int new_line_idx, char *buf);
+unsigned int	seek_new_line(char *buf, unsigned int *flag);
+char	*get_more_byte(char *buf, int fd, unsigned int *new_line_idx);
+char	*get_next_line(int fd);
 
 #endif
